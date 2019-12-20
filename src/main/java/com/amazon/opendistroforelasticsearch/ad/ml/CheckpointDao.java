12f7098157ef1807e05d81f3cfab513424876360
Lai <laijiang+opendistro@amazon.com>
2019-12-20 13:59:38 -0800

    Add async CheckpointDao methods.

--- src/main/java/com/amazon/opendistroforelasticsearch/ad/ml/CheckpointDao.java
+++ src/main/java/com/amazon/opendistroforelasticsearch/ad/ml/CheckpointDao.java
@@ -24,6 +24,7 @@ import java.util.Optional;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.delete.DeleteRequest;
 import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetRequest;
@@ -68,9 +69,12 @@ public class CheckpointDao {
     /**
      * Puts a model checkpoint in the storage.
      *
+     * @deprecated use putModelCheckpoint with listener instead
+     *
      * @param modelId Id of the model
      * @param modelCheckpoint Checkpoint data of the model
      */
+    @Deprecated
     public void putModelCheckpoint(String modelId, String modelCheckpoint) {
         Map<String, Object> source = new HashMap<>();
         source.put(FIELD_MODEL, modelCheckpoint);
@@ -85,11 +89,32 @@ public class CheckpointDao {
     }
 
     /**
+     * Puts a model checkpoint in the storage.
+     *
+     * @param modelId id of the model
+     * @param modelCheckpoint checkpoint of the model
+     * @param listener onResponse is called with null when the operation is completed
+     */
+    public void putModelCheckpoint(String modelId, String modelCheckpoint, ActionListener<Void> listener) {
+        Map<String, Object> source = new HashMap<>();
+        source.put(FIELD_MODEL, modelCheckpoint);
+        clientUtil
+            .<IndexRequest, IndexResponse>asyncRequest(
+                new IndexRequest(indexName, DOC_TYPE, modelId).source(source),
+                client::index,
+                ActionListener.wrap(r -> listener.onResponse(null), listener::onFailure)
+            );
+    }
+
+    /**
      * Returns the checkpoint for the model.
+     *
+     * @deprecated use getModelCheckpoint with listener instead
      *
      * @param modelId ID of the model
      * @return model checkpoint, or empty if not found
      */
+    @Deprecated
     public Optional<String> getModelCheckpoint(String modelId) {
         return clientUtil
             .<GetRequest, GetResponse>timedRequest(new GetRequest(indexName, DOC_TYPE, modelId), logger, client::get)
@@ -99,11 +124,52 @@ public class CheckpointDao {
     }
 
     /**
+     * Returns to listener the checkpoint for the model.
+     *
+     * @param modelId id of the model
+     * @param listener onResponse is called with the model checkpoint, or empty for no such model
+     */
+    public void getModelCheckpoint(String modelId, ActionListener<Optional<String>> listener) {
+        clientUtil
+            .<GetRequest, GetResponse>asyncRequest(
+                new GetRequest(indexName, DOC_TYPE, modelId),
+                client::get,
+                ActionListener.wrap(response -> listener.onResponse(processModelCheckpoint(response)), listener::onFailure)
+            );
+    }
+
+    private Optional<String> processModelCheckpoint(GetResponse response) {
+        return Optional
+            .ofNullable(response)
+            .filter(GetResponse::isExists)
+            .map(GetResponse::getSource)
+            .map(source -> (String) source.get(FIELD_MODEL));
+    }
+
+    /**
      * Deletes the model checkpoint for the id.
+     *
+     * @deprecated use deleteModelCheckpoint with listener instead
      *
      * @param modelId ID of the model checkpoint
      */
+    @Deprecated
     public void deleteModelCheckpoint(String modelId) {
         clientUtil.<DeleteRequest, DeleteResponse>timedRequest(new DeleteRequest(indexName, DOC_TYPE, modelId), logger, client::delete);
+    }
+
+    /**
+     * Deletes the model checkpoint for the model.
+     *
+     * @param modelId id of the model
+     * @param listener onReponse is called with null when the operation is completed
+     */
+    public void deleteModelCheckpoint(String modelId, ActionListener<Void> listener) {
+        clientUtil
+            .<DeleteRequest, DeleteResponse>asyncRequest(
+                new DeleteRequest(indexName, DOC_TYPE, modelId),
+                client::delete,
+                ActionListener.wrap(r -> listener.onResponse(null), listener::onFailure)
+            );
     }
 }

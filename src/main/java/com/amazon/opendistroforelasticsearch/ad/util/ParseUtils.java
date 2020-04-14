e9d4a460a3371ffd4a66330fc55e2d9492db546b
Vijay <vamshin@amazon.com>
2020-04-14 12:28:22 -0700

    fix spotless issues

--- src/main/java/com/amazon/opendistroforelasticsearch/ad/util/ParseUtils.java
+++ src/main/java/com/amazon/opendistroforelasticsearch/ad/util/ParseUtils.java
@@ -219,8 +219,7 @@ public final class ParseUtils {
                                 );
                             }
 
-                            aggBuilder = parser.namedObject(BaseAggregationBuilder.class, fieldName, aggregationName
-                            );
+                            aggBuilder = parser.namedObject(BaseAggregationBuilder.class, fieldName, aggregationName);
                     }
                 } else {
                     throw new ParsingException(

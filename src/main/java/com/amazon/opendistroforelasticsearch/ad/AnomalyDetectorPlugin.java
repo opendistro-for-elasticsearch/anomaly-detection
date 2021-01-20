4fdb80911bcbb59529ee78f1820761fb5a4ba562
Kaituo Li <kaituo@amazon.com>
2021-01-20 14:16:17 -0800

    backport PR 310: fix double nan error when parse to json

--- src/main/java/com/amazon/opendistroforelasticsearch/ad/AnomalyDetectorPlugin.java
+++ src/main/java/com/amazon/opendistroforelasticsearch/ad/AnomalyDetectorPlugin.java
@@ -87,6 +87,7 @@ import com.amazon.randomcutforest.serialize.RandomCutForestSerDe;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.elasticsearch.SpecialPermission;
 import org.elasticsearch.action.ActionRequest;
 import org.elasticsearch.action.ActionResponse;
@@ -239,7 +240,7 @@ public class AnomalyDetectorPlugin extends Plugin implements ActionPlugin, Scrip
     }
 
     private static Void initGson() {
-        gson = new Gson();
+        gson = new GsonBuilder().serializeSpecialFloatingPointValues().create();
         return null;
     }
 
